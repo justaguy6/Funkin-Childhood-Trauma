c80cd801317c2c09b984095e1a6c84afd4e3de07
Random people <98900217+justaguy6@users.noreply.github.com>
2023-09-05 08:56:32 -0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -12,7 +12,7 @@ import openfl.events.Event;
 import openfl.display.StageScaleMode;
 import lime.app.Application;
 import openfl.Lib;
-
+import lime.system.System;
 #if desktop
 import Discord.DiscordClient;
 #end
@@ -42,6 +42,9 @@ class Main extends Sprite
 	};
 
 	public static var fpsVar:FPS;
+
+	public static var path:String = System.applicationStorageDirectory;
+
 
 	// You can pretty much ignore everything from here on - your code should go in your states.
 
@@ -91,7 +94,7 @@ class Main extends Sprite
 		ClientPrefs.loadDefaultKeys();
 		addChild(new FlxGame(game.width, game.height, game.initialState, #if (flixel < "5.0.0") game.zoom, #end game.framerate, game.framerate, game.skipSplash, game.startFullscreen));
 
-		#if !mobile
+		#if android 
 		fpsVar = new FPS(10, 3, 0xFFFFFF);
 		addChild(fpsVar);
 		Lib.current.stage.align = "tl";
@@ -133,7 +136,7 @@ class Main extends Sprite
 		dateNow = dateNow.replace(" ", "_");
 		dateNow = dateNow.replace(":", "'");
 
-		path = "./crash/" + "PsychEngine_" + dateNow + ".txt";
+		path = Main.path + "crash/" + "PsychEngine_" + dateNow + ".txt";
 
 		for (stackItem in callStack)
 		{
@@ -148,8 +151,8 @@ class Main extends Sprite
 
 		errMsg += "\nUncaught Error: " + e.error + "\nPlease report this error to the GitHub page: https://github.com/ShadowMario/FNF-PsychEngine\n\n> Crash Handler written by: sqirra-rng";
 
-		if (!FileSystem.exists("./crash/"))
-			FileSystem.createDirectory("./crash/");
+		if (!Assets.exists(Main.path + "/crash/"))
+			FileSystem.createDirectory(Main.path + "/crash/");
 
 		File.saveContent(path, errMsg + "\n");
 
@@ -157,7 +160,6 @@ class Main extends Sprite
 		Sys.println("Crash dump saved in " + Path.normalize(path));
 
 		Application.current.window.alert(errMsg, "Error!");
-		DiscordClient.shutdown();
 		Sys.exit(1);
 	}
 	#end
